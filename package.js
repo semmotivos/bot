e43326eab784a700789327b57bb06f2c73fd8fef
semmotivos <fugakusayko@gmail.com>
2025-03-02 23:39:19 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+  "name": "akira-bot",
+  "version": "1.0.0",
+  "description": "Bot Akira para WhatsApp",
+  "main": "venom_bot.js",
+  "scripts": {
+    "start": "node venom_bot.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "venom-bot": "^4.0.0",
+    "axios": "^1.3.0"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  }
+}
